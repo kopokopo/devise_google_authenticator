f49f0ba09e510d05db6efd7580bf4a4f70d82fc1
DavidKar1uk1 <david.mwangi@kopokopo.com>
2025-01-21 08:04:26 +0300

    refactor(rails8.0) Allow usage within rails 8.0 apps

--- devise_google_authenticator.gemspec
+++ devise_google_authenticator.gemspec
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 
 Gem::Specification.new do |s|
   s.name = "devise_google_authenticator"
-  s.version = "1.0.0"
+  s.version = "1.1.0"
   s.authors = ["Christian Frichot"]
   s.date = "2015-02-08"
   s.description = "Devise Google Authenticator Extension, for adding Google's OTP to your Rails apps!"
@@ -23,7 +23,7 @@ Gem::Specification.new do |s|
     'devise'  => '>= 4.9.4',
     'rotp'    => '~> 6.3',
     'rqrcode' => '~> 2.2',
-    'rails' => '~> 7.2',
+    'rails' => '>= 7.2',
   }.each do |lib, version|
     s.add_runtime_dependency(lib, *version)
   end
--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,18 +1,14 @@
 class DeviseGoogleAuthenticatorAddTo<%= table_name.camelize %> < ActiveRecord::Migration
-  def self.up
-    change_table :<%= table_name %> do |t|
-      t.string  :gauth_secret
-      t.string  :gauth_enabled, :default => "f"
-      t.string  :gauth_tmp
-      t.datetime  :gauth_tmp_datetime
+  def change
+    change_table :users do |t|
+      t.string :gauth_tmp
+      t.string :gauth_secret
+      t.boolean :gauth_enabled, default: false
+      t.boolean :gauth_has_been_reset, default: false
+      t.boolean :google_auth_app_set_up, default: false
+      t.datetime :gauth_tmp_datetime
       t.datetime :remember_gauth_token_set_at
       t.datetime :remember_gauth_token_expires_at
-    end
-  end
-
-  def self.down
-    change_table :<%= table_name %> do |t|
-      t.remove :gauth_secret, :gauth_enabled, :gauth_tmp, :gauth_tmp_datetime, :remember_gauth_token_set_at, :remember_gauth_token_expires_at
     end
   end
 end
